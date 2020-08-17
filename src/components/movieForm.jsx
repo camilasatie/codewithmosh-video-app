b8c5b6f1e81ed71a31d67969b39d2d419977ac47
camilasatie <camilasatie.dev@gmail.com>
2020-08-16 22:41:39 -0300

    chore: calling backend movie service

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Joi from 'joi-browser'
-import Form from './common/form'
-import { getMovie, saveMovie } from '../services/fakeMovieService'
-import { getGenres } from '../services/fakeGenreService'
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import { getMovie, saveMovie } from "../services/movieService";
+import { getGenres } from "../services/genreService";
 
 class MovieForm extends Form {
   state = {
@@ -14,27 +14,49 @@ class MovieForm extends Form {
     },
     genres: [],
     errors: {}
-  }
+  };
 
   schema = {
     _id: Joi.string(),
-    title: Joi.string().required().label('Title'),
-    genreId: Joi.string().required().label('Genre'),
-    numberInStock: Joi.number().required().min(0).max(100).label('Number in Stock'),
-    dailyRentalRate: Joi.number().required().min(0).max(10).label('Rate'),
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genreId: Joi.string()
+      .required()
+      .label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate")
+  };
+
+  async populateGenres() {
+    const { data: genres } = await getGenres();
+    this.setState({ genres });
   }
 
-  componentDidMount() {
-    const genres = getGenres();
-    this.setState({ genres })
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
+  }
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace('/not-found')
-
-    this.setState({ data: this.mapToViewModel(movie) })
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
@@ -44,24 +66,24 @@ class MovieForm extends Form {
       genreId: movie.genre._id,
       numberInStock: movie.numberInStock,
       dailyRentalRate: movie.dailyRentalRate
-    }
+    };
   }
 
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
 
-    this.props.history.push('/movies')
-  }
+    this.props.history.push("/movies");
+  };
 
   render() {
     return (
       <div>
-        <h1>Movie Form </h1>
+        <h1>Movie Form</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput('title', 'Title')}
-          {this.renderSelect('genreId', 'Genre', this.state.genres)}
-          {this.renderInput('numberInStock', 'Number in Stock')}
-          {this.renderInput('dailyRentalRate', 'Rate')}
+          {this.renderInput("title", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
+          {this.renderInput("dailyRentalRate", "Rate")}
           {this.renderButton("Save")}
         </form>
       </div>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { NavLink } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import Pagination from "./common/pagination";
 import ListGroup from "./common/listGroup";
 import MoviesTable from "./moviesTable";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import { paginate } from "../utils/paginate";
 import _ from 'lodash'
-import SearchBox from "./common/SearchBox";
+import SearchBox from "./searchBox";
 
 class Movies extends Component {
   state = {
@@ -21,14 +22,27 @@ class Movies extends Component {
   };
 
   async componentDidMount() {
-    const { data } = await getGenres()
-    const genres = [{ _id: '', name: 'All Genres' }, ...data]
-    this.setState({ movies: getMovies(), genres: genres });
+    const { data } = await getGenres();
+    const genres = [{ _id: "", name: "All Genres" }, ...data];
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
-  handleDelete = (movie) => {
-    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+  handleDelete = async (movie) => {
+    const originalMovies = this.state.movies;
+    const movies = originalMovies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    }
+    catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error('This movie has already been deleted.')
+
+      this.setState({movies: originalMovies})
+    }
   };
 
   handleLike = (movie) => {
